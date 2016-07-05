bfa2c65510b8b9fc9bb7b2dcfd76767e4a3fee0e
Young-Hwan Mun <mcodegeeks@gmail.com>
2016-07-04 21:13:58 -0400

    Initial commit

---
+++ GifMake-ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifMake-ObjC
+//
+//  Created by Younghwan Mun on 2016-07-04.
+//  Copyright © 2016 MCodeGeeks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GifMake-ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GifMake-ObjC
+//
+//  Created by Younghwan Mun on 2016-07-04.
+//  Copyright © 2016 MCodeGeeks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
